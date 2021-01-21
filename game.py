9ee33dfbfbf42a7f3173468d59a841b919edd9bf
jermworm <fullstackmiguel@gmail.com>
2021-01-21 02:04:46 -0500

    first commit

---
+++ game.py
@@ -0,0 +1,13 @@
+import sys
+import random
+
+age = 32
+birthPlace = 'New York City'
+
+print('Please enter your name ')
+name = input()
+print('Hello ' + name + '!')
+print(name + ": Where am I?")
+print('Welcome to Emphoria. You have been requested by the King himself')
+print(name + ': Where?')
+print('Emphoria is home to the Gortians.')
